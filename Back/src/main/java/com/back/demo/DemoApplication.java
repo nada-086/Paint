f273467ee13812ec15565f69387472b2ba2573b0
yara00 <yarahossam222@gmail.com>
2021-12-07 15:37:46 +0200

    added updates

--- Back/src/main/java/com/back/demo/DemoApplication.java
+++ Back/src/main/java/com/back/demo/DemoApplication.java
@@ -1,5 +1,4 @@
-package com.back.demo;
-
+package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +7,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		System.out.println("HI");
 	}
 
 }
