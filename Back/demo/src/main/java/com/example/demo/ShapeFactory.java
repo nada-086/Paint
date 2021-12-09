a8fa555ee5f1c07437e47ae70421d5049c80b665
1nourhan <95703963+1nourhan@users.noreply.github.com>
2021-12-09 17:37:02 +0200

    Update ShapeFactory.java

--- Back/demo/src/main/java/com/example/demo/ShapeFactory.java
+++ Back/demo/src/main/java/com/example/demo/ShapeFactory.java
@@ -10,41 +10,78 @@ public class ShapeFactory {
         shapeType = type;
     }
 
-    public  static ArrayList createShape(String shapeType, int width, int length, int x, int y, int ID, String color) {
+    public  static ArrayList createShape(String shapeType, String[] s) {
         System.out.println("type is" + shapeType);
         if(shapeType == null) {
             return null;
         }
 
         if(shapeType.equalsIgnoreCase("Rectangle")) {
-            IShape newShape = new Rectangle(width, length, x, y, ID, color);
+            int length = Integer.parseInt(s[1]);
+             int width = Integer.parseInt(s[2]);
+             int x = Integer.parseInt(s[3]);
+            int y = Integer.parseInt(s[4]);
+            int ID = Integer.parseInt(s[5]);
+            String color = s[6];
+            IShape newShape = new Rectangle(length, width, x, y, ID, color);
             shapes.add(newShape);
             return shapes;
         }
         else if(shapeType.equalsIgnoreCase("Circle")) {
+           
+             int radius = Integer.parseInt(s[1]);
+             int x = Integer.parseInt(s[2]);
+            int y = Integer.parseInt(s[3]);
+              int ID = Integer.parseInt(s[4]);
+            String color = s[5];
             System.out.println("HERE");
-            IShape newShape = new Circle(width, x, y, ID, color);
+            IShape newShape = new Circle(radius, x, y, ID, color);
             shapes.add(newShape);
             System.out.println("AYO");
             return shapes;
         }
         else if(shapeType.equalsIgnoreCase("Ellipse")) {
-            IShape newShape = new Ellipse(width, length, x, y, ID, color);
+            int x = Integer.parseInt(s[1]);
+             int y = Integer.parseInt(s[2]);
+             int rx = Integer.parseInt(s[3]);
+            int ry = Integer.parseInt(s[4]);
+            int ID = Integer.parseInt(s[5]);
+            String color = s[6];
+            IShape newShape = new Ellipse(x, y, rx, ry, ID, color);
             shapes.add(newShape);
             return shapes;
         }
         else if(shapeType.equalsIgnoreCase("Triangle")) {
-            IShape newShape = new Triangle(width, length, x, y, ID, color);
+            int x1 = Integer.parseInt(s[1]);
+             int y1 = Integer.parseInt(s[2]);
+             int x2 = Integer.parseInt(s[3]);
+            int y2 = Integer.parseInt(s[4]);
+            int x3 = Integer.parseInt(s[5]);
+            int y3 = Integer.parseInt(s[6]);
+            int ID = Integer.parseInt(s[7]);
+            String color = s[8];
+            IShape newShape = new Triangle(x1, y1, x2, y2, x3, y3, ID, color);
             shapes.add(newShape);
             return shapes;
         }
         else if(shapeType.equalsIgnoreCase("Square")) {
+             int width = Integer.parseInt(s[1]);
+             int x = Integer.parseInt(s[2]);
+            int y = Integer.parseInt(s[3]);
+              int ID = Integer.parseInt(s[4]);
+            String color = s[5];
             IShape newShape = new Square(width, x, y, ID, color);
             shapes.add(newShape);
             return shapes;
         }
         else if(shapeType.equalsIgnoreCase("Line")) {
-            IShape newShape = new Line(x, y, ID, color);
+             int x1 = Integer.parseInt(s[1]);
+             int y1 = Integer.parseInt(s[2]);
+             int x2 = Integer.parseInt(s[3]);
+            int y2 = Integer.parseInt(s[4]);
+            int ID = Integer.parseInt(s[5]);
+            String color = s[6];
+            IShape newShape = new Line(x1, y1, x2, y2 ID, color);
             shapes.add(newShape);
             System.out.println("size is " + shapes.size());
             return shapes;
